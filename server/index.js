10135e3c10109b661e31dd353ec330e509fe3097
Fene-87 <fenekayas87@gmail.com>
2022-08-25 22:15:50 -0900

    can update and delete employees

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ const db = mysql2.createConnection({
     database: 'employeedatabase',
 });
 
+
+
 app.post('/create', (req, res) => {
     const name = req.body.name;
     const age = req.body.age;
@@ -28,7 +30,49 @@ app.post('/create', (req, res) => {
         } else{
             res.send("values inserted");
         }
-    })
+    });
+});
+
+app.get('/employees', (req, res) => {
+    db.query("SELECT * FROM employees", (err, result) => {
+        if(err){
+            console.log(err);
+        }else {
+            res.send(result);
+        }
+    });
+});
+
+app.put('/update', (req, res) => {
+    const id = req.body.id;
+    const wage = req.body.wage;
+
+    db.query(
+        'UPDATE employees SET wage = ? WHERE id = ?',
+        [wage, id],
+        (err, result) => {
+            if(err){
+                console.log(err);
+            }else {
+                res.send(result);
+            }
+        }
+        )
+})
+
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.query(
+        'DELETE FROM employees WHERE id = ?', id,
+        (err, result) => {
+            if(err){
+                console.log(err);
+            }else {
+                res.send(result);
+            }
+        }
+    )
 })
 
 app.listen(3001, () => {
